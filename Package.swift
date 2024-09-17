efa8da06b051a159f04219f4ab1fc13b98c14bf9
lahari <gplahari@gmail.com>
2024-09-17 18:32:58 +0200

    Update packages

--- Package.swift
+++ Package.swift
@@ -26,22 +26,24 @@ let package = Package(
         .library(
             name: "Kitura-WebSocket",
             targets: ["KituraWebSocket"]),
-        ],
+    ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
         .package(url: "https://github.com/StyleShoots/Kitura-net.git", branch: "master"),
         .package(url: "https://github.com/Kitura/BlueCryptor.git", from: "2.0.0"),
-
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target defines a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "KituraWebSocket",
-            dependencies: ["KituraNet", "Cryptor"]),
+            dependencies: [
+                .product(name: "KituraNet", package: "Kitura-net"),
+                .product(name: "Cryptor", package: "BlueCryptor")
+            ]
+        ),
         .testTarget(
             name: "KituraWebSocketTests",
             dependencies: ["KituraWebSocket"]),
-        ]
+    ]
 )
